c829ab4791727722dc673ff591221ecf68e05f07
tw7613781 <tw7613781@gmail.com>
2018-01-22 03:59:55 +0900

    implement user login

---
+++ user.py
@@ -0,0 +1,36 @@
+class User:
+
+    def __init__(self,email):
+        '''
+        initialize user class with email as user unique identifier
+        :param email:
+        '''
+        self.email = email
+
+    def get_id(self):
+        '''
+        return the unique identifier for the user which is email
+        :return: user's email
+        '''
+        return self.email
+
+    def is_active(self):
+        '''
+        we asume all created users are active
+        :return: boolean
+        '''
+        return True
+
+    def is_anonymous(self):
+        '''
+        we regards all created users are not anonymous
+        :return: boolean
+        '''
+        return False
+
+    def is_authenticated(self):
+        '''
+        all created users are authenticated as we only create the user object after the correct username and passwrod is entered
+        :return: boolean
+        '''
+        return True
--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,45 @@
-from flask import Flask
+# coding:utf-8
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, login_required, login_user, logout_user
+
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+
+DB = DBHelper()
 
 app = Flask(__name__)
+app.secret_key = 'vwJFwvqaFZIkqqkqE4PWGFTQyWvOfbI/aqOSshz/XQbh/PX3cZfVKBqwk59QiUp79h/lg410yvB9Vldynz1yL9qTBiz/EWqtCQi'
+login_manager = LoginManager(app)
 
 @app.route('/')
 def home():
-    return 'Under construction'
+    return render_template('home.html')
+
+@app.route('/account')
+@login_required
+def account():
+    return 'You are logged in'
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user_password = DB.get_user(email)
+    if user_password and user_password == password:
+        user = User(email)
+        login_user(user,remember=True)
+        return redirect(url_for('account'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
