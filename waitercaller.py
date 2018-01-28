e7fd603137a254a72e560a2ba0a13ea2ee93919c
tw7613781 <tw7613781@gmail.com>
2018-01-28 21:27:39 +0900

    favicon and db duplicate index

--- waitercaller.py
+++ waitercaller.py
@@ -88,8 +88,10 @@ def account_deletetable():
 
 @app.route('/newrequest/<tid>')
 def new_request(tid):
-    DB.add_request(tid, datetime.datetime.now())
-    return 'Your request has been logged and a waiter will be with you shortly'
+    if DB.add_request(tid, datetime.datetime.now()):
+        return 'Your request has been logged and a waiter will be with you shortly'
+    else:
+        return "There is already a request pending for this table. Please be patient, a waiter will be there ASAP"
 
 @app.route('/dashboard')
 @login_required
